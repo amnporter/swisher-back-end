242af90fedb5ab46fe0311c3f5756aca2ed2430c
Porter, Chris <amnporter@gmail.com>
2020-08-02 14:53:02 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, swaggerDoc);
   /* End swagger module */
   
-  await app.listen(process.env.port || 3000);
+  await app.listen(process.env.port);
 }
 bootstrap();
